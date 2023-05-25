cac2b0741f0c1dcf151edfeff458d28e39bda5c5
Yingyang Zhang <yingyang.zhang95@gmail.com>
2023-05-25 10:32:15 -0400

    added dashboard HTML

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,13 @@
 import '../index.css';
 import Header from './Header';
+import Dashboard from '../Components/Dashboard/Dashboard';
 
 export default function App() {
     return (
         <div className="app container">
             <Header />
+        
+            <Dashboard />
         </div>
     )
 }
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,49 @@
 import React from "react";
+import character from './Images/character.png';
 
 export default function Dashboard() {
-    
+    return (
+        <div className="dashboard-container">
+            <div className="welcome-conatiner">
+                <p>Hey, Ethan</p>
+                <h1>Wwelcome Back.</h1>
+
+                <div className="img-container">
+                    <img src={character} alt="image" />
+                </div>
+            </div>
+
+            <div className="notifications-container">
+                <p className="title">Notifications</p>
+
+                <div className="notification">
+                    <p className="date">2023-05-19</p>
+                    
+                    <div className="notification-objects">
+                        <p className="notification-text">You have a new message from John Smith</p>
+                        <div className="unread">
+                            <p>Unread</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="messages container">
+                <p className="title">Messages</p>
+
+                <div className="message">
+                    <p className="message-text">Perfect! I'll send you a calendar invite. Thanks, Emily!</p>
+
+                    <div className="message-objects">
+                        <div className="user">
+                            <span>E</span>
+                            <p>Emily Johnson</p>
+                        </div>
+
+                        <div className="time">11:20</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
